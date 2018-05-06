a88a08e44cdf4e24de102fe97b7b68c8e23cb497
Nicolas Morel <nicolas@morel.io>
2018-05-06 16:08:43 +0200

    Fix coverage on transformed files. Fixes #826.

---
+++ test/coverage/transformed.js
@@ -0,0 +1,10 @@
+'use strict';
+exports.__esModule = true;
+// Load modules
+// Declare internals
+var internals = {};
+function method(value) {
+    return value;
+}
+exports.method = method;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsWUFBWSxDQUFDOztBQUViLGVBQWU7QUFHZixvQkFBb0I7QUFFcEIsSUFBTSxTQUFTLEdBQUcsRUFBRSxDQUFDO0FBR3JCLGdCQUF1QixLQUFLO0lBRXhCLE9BQU8sS0FBSyxDQUFDO0FBQ2pCLENBQUM7QUFIRCx3QkFHQyJ9
