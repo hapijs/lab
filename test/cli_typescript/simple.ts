8cccdd002cf3893a2cc6d805c8334160365b9bc1
Eran Hammer <eran@hammer.io>
2022-01-14 00:44:42 -0800

    Skip coverage on non-mapped transpiled code

--- test/cli_typescript/simple.ts
+++ test/cli_typescript/simple.ts
@@ -1,17 +1,20 @@
 import { expect } from '@hapi/code';
 import * as _Lab from '../../test_runner';
 
+import { add } from './api';
+
+
 const { describe, it } = exports.lab = _Lab.script();
 
 describe('Test CLI', () => {
 
     it('adds two numbers together', () => {
 
-        expect(1 + 1).to.equal(2);
+        expect(add(1, 1)).to.equal(2);
     });
 
     it('subtracts two numbers', () => {
 
-        expect(2 - 2).to.equal(0);
+        expect(add(2, - 2)).to.equal(0);
     });
 });
