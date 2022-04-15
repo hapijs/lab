9ec1555632e1e7aaf51ce3faa473c049a0e638d1
Danilo Alonso <danilo@alonso.network>
2022-04-15 15:11:07 -0400

    feat: 🎸 depedencies cli option

---
+++ test/cli_tsconfig_paths/simple.ts
@@ -0,0 +1,22 @@
+import 'tsconfig-paths/register';
+
+import { expect } from '@hapi/code';
+import * as _Lab from '../../test_runner';
+
+import { add } from 'nested/api';
+
+
+const { describe, it } = exports.lab = _Lab.script();
+
+describe('Test CLI', () => {
+
+    it('adds two numbers together', () => {
+
+        expect(add(1, 1)).to.equal(2);
+    });
+
+    it('subtracts two numbers', () => {
+
+        expect(add(2, - 2)).to.equal(0);
+    });
+});
